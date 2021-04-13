0c0834032ba9b725a73855ce1f49dbb41aa1fc3e
Crystal Jeanne Ginete <82429449+jeanne2218@users.noreply.github.com>
2021-04-13 12:20:01 +0800

    Finish README

---
+++ 03_personal-info.py
@@ -0,0 +1,6 @@
+fN = input("First name: ")
+lN = input("Last name: ")
+loc = input("Location: ")
+age = input("Age: ")
+space = " "
+print("Hello, I'm"+space+fN+space+lN+space+"from"+space+loc+space+"age of"+space+age+"!")
